118ee505ed507354cdef6ffc6dd1fedebe554869
B1Classe <137699544+B1Classe@users.noreply.github.com>
2023-06-25 13:12:00 +0000

    Pending changes exported from your codespace

--- guides/example-server/README.md
+++
@@ -1,3 +0,0 @@
-# Example Server
-
-This guide demonstrates how to use {ruby Localhost::Authority} to implement a simple HTTPS client & server.
--- lib/localhost.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2018-2023, by Samuel Williams.
-
-require_relative 'localhost/version'
-require_relative 'localhost/authority'
